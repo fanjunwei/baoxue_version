281890248b95be057506416492d818fab511293e
fanjunwei <fanjunwei9140@gmail.com>
2014-04-17 11:11:28 +0800

    检索优化

--- version/views.py
+++ version/views.py
@@ -424,9 +424,9 @@ def browseVersionForTemplates(request):
 
         start= datetime.date.today()
         if today:
-                list=VBrows.objects.filter(Q(version_createTime__gt=start)&(Q(fullName__icontains=keyword)|Q(parentFullName__icontains=keyword)|Q(description__icontains=keyword)))
+                list=VBrows.objects.filter(Q(version_createTime__gt=start)&(Q(fullName__icontains=keyword)|Q(parentFullName__icontains=keyword)|Q(description__icontains=keyword)|Q(subbranch_description__icontains=keyword)|Q(branch_description__icontains=keyword)))
         else:
-            list=VBrows.objects.filter(Q(fullName__icontains=keyword)|Q(parentFullName__icontains=keyword)|Q(description__icontains=keyword))
+            list=VBrows.objects.filter(Q(fullName__icontains=keyword)|Q(parentFullName__icontains=keyword)|Q(description__icontains=keyword)|Q(subbranch_description__icontains=keyword)|Q(branch_description__icontains=keyword))
         result=[]
         paginator=Paginator(list,50)
         try:
